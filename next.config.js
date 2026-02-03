82702d2701c93f13557c71294dc51da17c5162dc
Christoph Hart <>
2026-02-03 10:15:34 +0100

    Revert "force vercel cache invalidation"
    This reverts commit 20b6f86947caefdf2a32062d356ee29ca62a8690.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Force cache invalidation on deploy - increment to bust cache
-  generateBuildId: async () => {
-    return `build-${Date.now()}`
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
